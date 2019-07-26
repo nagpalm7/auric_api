52a733205a8b9e8cedb25b5dcd8ba1c098df9211
Mohit Nagpal <nagpalm7@gmail.com>
2019-07-26 11:34:26 +0530

    setup postgresql on server

--- auric/settings/local.py
+++ auric/settings/local.py
@@ -3,3 +3,13 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*'] #['your_local_domain']  
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- auric/settings/production.py
+++ auric/settings/production.py
@@ -3,3 +3,17 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*'] #['your_local_domain']  
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'auric',
+        'USER': 'auricuser',
+        'PASSWORD': 'Dogu123!@#',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
